1b5b3ccf4d7c828ee18fc732cc68285c4e00ab0d
cliffordsugijanto <cliff_274@hotmail.com>
2015-02-18 13:00:44 +0700

    Project eJuklak version 0.1alpha

--- eJuklakApp/gen/com/example/ejuklak/BuildConfig.java
+++ eJuklakApp/gen/com/example/ejuklak/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.ejuklakapp;
+package com.example.ejuklak;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
